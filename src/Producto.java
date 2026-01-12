9c9b815a0b7d8ff483eb66f31c3fa24bdb69f981
Dusty <dcpanes@gmail.com>
2026-01-12 19:33:10 -0300

    producto class

---
+++ src/Producto.java
@@ -0,0 +1,15 @@
+public class Producto {
+    private String id;
+    private String nombre;
+    private Double precio;
+
+    public Producto(
+        String id,
+        String nombre,
+        Double precio
+    ){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
